c7c0475d58f088d10050c42d500bf50a95d3057a
Marvin Otec <motec@in-technology.eu>
2020-09-23 08:21:36 +0200

    Updates

---
+++ precache-manifest.20ce5d63b6a77546f4d2afe9adc9cf10.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4669dac1efc783b0e4d1db02326d5f2",
+    "url": "/mym/index.html"
+  },
+  {
+    "revision": "7d621e5b120ae38f58d2",
+    "url": "/mym/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9dffc77d15c720f14c7c",
+    "url": "/mym/static/js/2.c7692138.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mym/static/js/2.c7692138.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d621e5b120ae38f58d2",
+    "url": "/mym/static/js/main.f1786ab5.chunk.js"
+  },
+  {
+    "revision": "fec17eb48bd02f722d69",
+    "url": "/mym/static/js/runtime-main.bf4f651b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mym/static/media/logo.5d5d9eef.svg"
+  }
+]);
