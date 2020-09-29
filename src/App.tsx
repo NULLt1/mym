ef315b75253c2e5c6c81cc29d8b6a92f70264a08
Marvin Otec <motec@in-technology.eu>
2020-09-29 13:49:26 +0200

    typo

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hier entsteht der Webauftriff von MYM</h1>
+      <h1>Hier entsteht der Webauftritt von MYM</h1>
     </div>
   );
 }
